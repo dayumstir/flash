684cda3d4566632bf754daf708f8abc7864ccbb3
dayumstir <dempstertay123@gmail.com>
2022-06-08 12:18:48 +0800

    pre testing

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ import RegisterScreen from './components/auth/Register';
 import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main'
 import AddScreen from './components/main/Add'
-
+import SaveScreen from './components/main/Save'
 
 const Stack = createStackNavigator();
 
@@ -94,7 +94,8 @@ export class App extends Component {
           <Stack.Navigator initialRouteName="Main">
 
             <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
-            <Stack.Screen name="Add" component={AddScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Save" component={SaveScreen} />
 
           </Stack.Navigator>
         </NavigationContainer>
